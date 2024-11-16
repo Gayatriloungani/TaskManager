4c951a4c3bbe86cddaf474fb9cc8e14bd54ce5a4
Gayatri Loungani <97401692+Gayatriloungani@users.noreply.github.com>
2024-11-16 12:17:06 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://task-manager-api-delta.vercel.app/';
+export const API_URL = 'https://task-manager-api-delta.vercel.app';
